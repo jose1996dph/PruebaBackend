967c5c0cec4419e4e12013dbdb97aba8bb5ffbf4
jose.perez@intelligisgroup.com <jose.perez@intelligisgroup.com>
2021-05-25 11:33:56 -0400

    Data repository from files

---
+++ Data/FileRepository.cs
@@ -0,0 +1,19 @@
+﻿using Data.Interfaces;
+using Framework;
+using System;
+
+namespace Data
+{
+    public class FileRepository : IFileRepository
+    {
+        readonly FileStore fileStore;
+        public FileRepository()
+        {
+            fileStore = new FileStore();
+        }
+        public void Save(string file, string name)
+        {
+            fileStore.SaveFile(file, name);
+        }
+    }
+}
