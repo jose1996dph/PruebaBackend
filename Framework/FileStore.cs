d7b7bf84286fc5ed5a17e8c1e61e316eed633bbe
jose.perez@intelligisgroup.com <jose.perez@intelligisgroup.com>
2021-05-25 15:04:38 -0400

    Create file

--- Framework/FileStore.cs
+++ Framework/FileStore.cs
@@ -7,7 +7,13 @@ namespace Framework
     {
         public void SaveFile(string base64, string name)
         {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Files\\", name);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Files\\");
+            var filePath = Path.Combine(path, name);
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
 
             var bytes = Convert.FromBase64String(base64);
             File.WriteAllBytes(filePath, bytes);
